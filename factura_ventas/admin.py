418ae28cd46ff87e60e2f35108516a889f94cfcf
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-15 15:11:07 -0500

    Update admin.py

--- factura_ventas/admin.py
+++ factura_ventas/admin.py
@@ -7,5 +7,5 @@ from factura_ventas.models import Factura_Venta
 @admin.register(Factura_Venta)
 
 class FacturaVentaAdmin(admin.ModelAdmin):
-    list_display=['id','cliente','fecha_ingreso','medio_pago_v','estado_pago_v','total_v']
+    list_display=['id','cliente','fecha_ingreso','medio_pago_v','estado_pago_v']
     
