cf0b7d96c167636284f16cb8b83b74b7e5fa83b5
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-15 15:11:23 -0500

    Update serializers.py

--- factura_ventas/api/serializers.py
+++ factura_ventas/api/serializers.py
@@ -4,4 +4,4 @@ from factura_ventas.models import Factura_Venta
 class FacturaVentasSerializer(serializers.ModelSerializer):
     class Meta:
         model=Factura_Venta
-        fields=['id','cliente','fecha_ingreso','medio_pago_v','estado_pago_v','total_v']
+        fields=['id','cliente','fecha_ingreso','medio_pago_v','estado_pago_v']
