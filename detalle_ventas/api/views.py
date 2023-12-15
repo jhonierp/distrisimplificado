8b4e010c9ded9d955c3f7c5fa32092b4f07d6cde
jhonierp <jhonierpasos9@gmail.com>
2023-12-15 17:51:28 -0500

    filtro de factura

--- detalle_ventas/api/views.py
+++ detalle_ventas/api/views.py
@@ -4,8 +4,17 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from detalle_ventas.models import Detalle_Venta
 from detalle_ventas.api.serializers import DetalleVentasSerializer
 
+#filtro
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class DetalleVentasViewSet(ModelViewSet):
     permission_classes=[IsAuthenticatedOrReadOnly]
     serializer_class=DetalleVentasSerializer
     queryset=Detalle_Venta.objects.all()
+#filtro
+    filter_backends=[DjangoFilterBackend,OrderingFilter]
+    filterset_fields=['categoria']
+    ordening_fields='__all__'
+    
