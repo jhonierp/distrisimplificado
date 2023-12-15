386593565e327301e7f3d428028293db0eee3104
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-15 15:20:20 -0500

    Update models.py

--- detalle_ventas/models.py
+++ detalle_ventas/models.py
@@ -5,7 +5,7 @@ class Detalle_Venta(models.Model):
     factura_venta=models.ForeignKey('factura_ventas.Factura_Venta',on_delete=models.SET_NULL,null=True,blank=True)
     producto_venta=models.ForeignKey('productos.Producto',on_delete=models.SET_NULL,null=True,blank=True)
     cantidad = models.PositiveIntegerField(null=True)
-    precio_producto=models.DecimalField(max_digits=10,decimal_places=2,null=True)  
+ 
     
     def __int__(self):
         return self.producto_venta
