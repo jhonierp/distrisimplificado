69f374076c503905335b67feef7d2e3c34c5c0a9
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-16 17:02:31 -0500

    Update models.py

--- stock/models.py
+++ stock/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Stock(models.Model):
-    producto_stock=models.ForeignKey('productos.Producto',on_delete=models.SET_NULL,null=True,blank=True)
-    lote_stock=models.ForeignKey('lotes.Lote',on_delete=models.SET_NULL,null=True,blank=True)
+    producto_estante=models.ForeignKey('productos.Producto',on_delete=models.SET_NULL,null=True,blank=True)
+    lote_estante=models.ForeignKey('lotes.Lote',on_delete=models.SET_NULL,null=True,blank=True)
     
     def __int__(self):
         return self.cantidad
