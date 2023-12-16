21e449a66b244beba74cbaa50077783196683c7b
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-16 17:02:50 -0500

    Update admin.py

--- stock/admin.py
+++ stock/admin.py
@@ -7,5 +7,5 @@ from stock.models import Stock
 @admin.register(Stock)
 
 class StockAdmin(admin.ModelAdmin):
-    list_display=['id','producto_stock','lote_stock']
+    list_display=['id','producto_estante','lote_estante']
     
