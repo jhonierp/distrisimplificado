ffbbef8683f18277b4b46b2c020bd06feb065eb7
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-17 10:34:58 -0500

    Update admin.py

--- factura_compras/admin.py
+++ factura_compras/admin.py
@@ -7,5 +7,5 @@ from factura_compras.models import Factura_Compra
 @admin.register(Factura_Compra)
 
 class FacturaCompraAdmin(admin.ModelAdmin):
-    list_display=['id','proveedor','telefono','fecha_ingreso','email']
+    list_display=['id','proveedor','telefono','fecha_ingreso','email','categoriaf']
     
