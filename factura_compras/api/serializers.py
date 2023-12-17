c91997a60a8fde8bb21e25d96877fd774c699a2c
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-17 10:35:10 -0500

    Update serializers.py

--- factura_compras/api/serializers.py
+++ factura_compras/api/serializers.py
@@ -4,4 +4,4 @@ from factura_compras.models import Factura_Compra
 class FacturaComprasSerializer(serializers.ModelSerializer):
     class Meta:
         model=Factura_Compra
-        fields=['id','proveedor','telefono','fecha_ingreso','email']
+        fields=['id','proveedor','telefono','fecha_ingreso','email','categoriaf']
