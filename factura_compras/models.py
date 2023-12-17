54324c5db57632e9b50a89c99a076e92c5ec29dd
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-17 09:55:41 -0500

    Update models.py

--- factura_compras/models.py
+++ factura_compras/models.py
@@ -2,23 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-medio_pago = (
-    ("Efectivo", "Efectivo"),
-    ("Tarjeta de Crédito", "Tarjeta de Crédito"),
-    ("Transferencia Bancaria", "Transferencia Bancaria"),
-)
-estado_pago = (
-        ('PENDIENTE', 'Pendiente'),
-        ('APROBADO', 'Aprobado'),
-        ('RECHAZADO', 'Rechazado'),
-)
+
 
 class Factura_Compra(models.Model):
     fecha_ingreso=models.DateField()
     proveedor = models.CharField(max_length=100,null=True)
     telefono = models.CharField(max_length=150)
-    medio_pago_c=models.CharField(max_length=255,choices=medio_pago)
-    estado_pago_c=models.CharField(max_length=255,choices=estado_pago)
+    email = models.CharField(max_length=100,null=True)
     
     
     def __int__(self):
