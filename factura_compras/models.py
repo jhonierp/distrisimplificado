df6f859096efba3dbc2d62b2a6ec94a6a4f35ab6
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-17 10:51:36 -0500

    Update models.py

--- factura_compras/models.py
+++ factura_compras/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
-
+medio_pago = (
+    ("Efectivo", "Efectivo"),
+    ("Tarjeta de Crédito", "Tarjeta de Crédito"),
+    ("Transferencia Bancaria", "Transferencia Bancaria"),
+)
+estado_pago = (
+        ('PENDIENTE', 'Pendiente'),
+        ('APROBADO', 'Aprobado'),
+        ('RECHAZADO', 'Rechazado'),
+)
 
 
 class Factura_Compra(models.Model):
@@ -10,7 +19,9 @@ class Factura_Compra(models.Model):
     telefono = models.CharField(max_length=150)
     email = models.CharField(max_length=100,null=True)
     categoriaf = models.ForeignKey('categorias.Categoria',on_delete=models.SET_NULL,null=True,blank=True)
-    
+    medio_pago_c=models.CharField(max_length=255,choices=medio_pago)
+    estado_pago_c=models.CharField(max_length=255,choices=estado_pago)
+    lote_f = models.ForeignKey('lotes.Lote',on_delete=models.SET_NULL,null=True,blank=True)
     
     def __int__(self):
         return self.total_c
