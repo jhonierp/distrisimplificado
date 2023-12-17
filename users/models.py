041497b3767eaa2f007a955532edb77ec768086f
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-17 10:48:31 -0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    pass
+   """ 
     email=models.EmailField(unique=True)    
     USERNAME_FIELD="email"
     REQUIRED_FIELDS=[]
     
-"""
+
 
 Para crear un Usuario por primera vez debe de estar asi:
 
