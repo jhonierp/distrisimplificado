1d0d01437fc39c8f2e54d90c3c75476b61985d38
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-12-17 10:34:40 -0500

    Update models.py

--- factura_compras/models.py
+++ factura_compras/models.py
@@ -9,6 +9,7 @@ class Factura_Compra(models.Model):
     proveedor = models.CharField(max_length=100,null=True)
     telefono = models.CharField(max_length=150)
     email = models.CharField(max_length=100,null=True)
+    categoriaf = models.ForeignKey('categorias.Categoria',on_delete=models.SET_NULL,null=True,blank=True)
     
     
     def __int__(self):
